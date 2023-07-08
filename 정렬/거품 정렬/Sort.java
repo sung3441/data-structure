f4338578b77b5e9034b114a0329f1f42d4d01488
SUNGIL CHO <67699933+sung3441@users.noreply.github.com>
2023-07-08 16:10:14 +0900

    Create Sort.java

---
+++ 정렬/거품 정렬/Sort.java
@@ -0,0 +1,24 @@
+public class Sort {
+    public static void sort(int[] a) {
+        sort(a, a.length);
+    }
+
+    private static void sort(int[] a, int size) {
+        // 라운드는 배열 크기 - 1 만큼 진행
+        for(int i = 1; i < size; i++) {'
+
+            // 각 라운드별 비교 횟수는 배열 크기의 현재 라운드를 뺀 만큼 비교
+            for(int j = 0; j < size - i; j++) {
+                if(a[j] > a [j + 1]) {
+                    swap(a, j, j + 1);
+                }
+            }
+        }
+    }
+    
+    private static void swap(int[] a, int i, int j) {
+        int temp = a[i];
+        a[i] = a[j];
+        a[j] = temp;
+    }
+}
